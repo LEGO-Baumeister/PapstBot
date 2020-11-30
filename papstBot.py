f10e3c163f1edd4966b6243ec03f777f6d1156b4
LEGO_Baumeister <lukas.severin.breuer@gmail.com>
2020-11-30 21:01:45 +0100

    prayers are now in a new file

--- papstBot.py
+++ papstBot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands, tasks
 from random import choice
+
+import prayers
 from secrets import token
 
 client = commands.Bot(command_prefix='+')
@@ -12,7 +14,7 @@ null = ["Bedenke, Mensch, dass du Staub bist und wieder zum Staub zurückkehren 
 @client.event
 async def on_ready():
     change_status.start()
-    print("Unsere hilfe ist im Namen des Herrn\n")
+    print("Unsere hilfe ist im Namen des Herrn")
     print("Der Himmel und Erde erschaffen hat.")
 
 
@@ -23,8 +25,12 @@ async def pray(ctx, arg1=None, arg2=None):
     credo = ["credo", "glaubensbekenntnis", "creed"]
     ave_maria = ["avemaria", "am", "gegrüßetseistdumaria", "hailmary"]
 
+    embed = discord.Embed(title="Command Help")
+    embed.add_field(name="Nutze:",value="+bete < paternoster | credo | avemaria > < de | la | en>")
+
     if arg1 == None:
         await ctx.send(choice(null), tts=True)
+        await ctx.send(embed=embed)
     elif arg1.lower() in pater_noster:
         await pater_noster_fun(ctx=ctx, voice=False, iso=arg2)
     elif arg1.lower() in credo:
@@ -69,75 +75,30 @@ async def pater_noster_fun(ctx, voice: bool, iso: str):
         pass
     else:
         if iso == "de":
-            await ctx.send("""Vater unser im Himmel, geheiligt werde dein Name.
-Dein Reich komme.
-Dein Wille geschehe,
-wie im Himmel so auf Erden.
-Unser tägliches Brot gib uns heute.""", tts=True)
-            await ctx.send("""Und vergib uns unsere Schuld,
-wie auch wir vergeben unsern Schuldigern.
-Und führe uns nicht in Versuchung,
-sondern erlöse uns von dem Bösen.""", tts=True)
-            await ctx.send("""Denn dein ist das Reich und die Kraft
-und die Herrlichkeit in Ewigkeit.
-Amen.""", tts=True)
+            await ctx.send(prayers.vater_unser_de[0], tts=True)
+            await ctx.send(prayers.vater_unser_de[1], tts=True)
+            await ctx.send(prayers.vater_unser_de[2], tts=True)
         elif iso == "la":
-            await ctx.send("""Pater noster, qui es in caelis:
-sanctificetur nomen tuum.
-Adveniat regnum tuum.
-Fiat voluntas tua, sicut in caelo, et in terra.
-Panem nostrum catidianum da nobis hodie""", tts=True)
-            await ctx.send("""Et dimitte nobis debita nostra,
-sicut et nos dimittimus debitoribus nostris.
-Et ne nos inducas in tentationem,
-sed libera nos a malo.""", tts=True)
-            await ctx.send("""Quia tuum est regnum et potestas et gloria
-in saecula.
-Amen.""", tts=True)
+            await ctx.send(prayers.vater_unser_la[0], tts=True)
+            await ctx.send(prayers.vater_unser_la[1], tts=True)
+            await ctx.send(prayers.vater_unser_la[2], tts=True)
         elif iso == "en":
-            await ctx.send("""Our Father, who art in heaven, hallowed be thy name;
-thy kingdom come; thy will be done on earth as it is in heaven.
-Give us this day our daily bread;""", tts=True)
-            await ctx.send("""and forgive us our trespasses,
-as we forgive those who trespass against us;
-and lead us not into temptation,
-but deliver us from evil.""", tts=True)
-            await ctx.send("""For the kingdom,
-the power and the glory are yours
-now and for ever.
-Amen.""", tts=True)
+            await ctx.send(prayers.vater_unser_en[0], tts=True)
+            await ctx.send(prayers.vater_unser_en[1], tts=True)
+            await ctx.send(prayers.vater_unser_en[2], tts=True)
 
 async def ave_maria_fun(ctx, voice: bool, iso: str):
     if voice:
         pass
     else:
         if iso == "de":
-            await ctx.send("""Gegrüßet seist du, Maria, voll der Gnade,
-der Herr ist mit dir.
-Du bist gebenedeit unter den Frauen,
-und gebenedeit ist die Frucht deines Leibes, Jesus.""", tts=True)
-            await ctx.send("""Heilige Maria, Mutter Gottes,
-bitte für uns Sünder
-jetzt und in der Stunde unseres Todes.
-Amen.""", tts=True)
+            await ctx.send(prayers.ave_maria_de[0], tts=True)
+            await ctx.send(prayers.ave_maria_de[1], tts=True)
         elif iso == "la":
-            await ctx.send("""Ave Maria, gratia plena,
-Dominus tecum.
-Benedicta tu in mulieribus,
-et benedictus fructus ventris tui, Iesus.""", tts=True)
-            await ctx.send("""Sancta Maria, Mater Dei,
-ora pro nobis peccatoribus
-nunc et in hora mortis nostrae.
-Amen""", tts=True)
+            await ctx.send(prayers.ave_maria_la[0], tts=True)
+            await ctx.send(prayers.ave_maria_la[1], tts=True)
         elif iso == "en":
-            await ctx.send("""Hail Mary, full of grace,
-the Lord is with thee.
-Blessed art thou amongst women,
-and blessed is the fruit of thy womb, Jesus.""", tts=True)
-            await ctx.send("""Holy Mary, Mother of God,
-pray for us sinners,
-now and at the hour of our death.
-Amen.""", tts=True)
-
+            await ctx.send(prayers.ave_maria_en[0], tts=True)
+            await ctx.send(prayers.ave_maria_en[1], tts=True)
 
 client.run(token)
---
+++ prayers.py
@@ -0,0 +1,98 @@
+vater_unser_de = ["""Vater unser im Himmel, 
+geheiligt werde dein Name.
+Dein Reich komme.
+Dein Wille geschehe,
+wie im Himmel so auf Erden.
+Unser tägliches Brot gib uns heute.""", """Und vergib uns unsere Schuld,
+wie auch wir vergeben unsern Schuldigern.
+Und führe uns nicht in Versuchung,
+sondern erlöse uns von dem Bösen.""", """Denn dein ist das Reich und die Kraft
+und die Herrlichkeit in Ewigkeit.
+Amen."""]
+
+vater_unser_la = ["""Pater noster, qui es in caelis:
+sanctificetur nomen tuum.
+Adveniat regnum tuum.
+Fiat voluntas tua, sicut in caelo, et in terra.
+Panem nostrum catidianum da nobis hodie""", """Et dimitte nobis debita nostra,
+sicut et nos dimittimus debitoribus nostris.
+Et ne nos inducas in tentationem,
+sed libera nos a malo.""", """Quia tuum est regnum et potestas et gloria
+in saecula.
+Amen."""]
+
+vater_unser_en = ["""Our Father, who art in heaven, hallowed be thy name;
+thy kingdom come; thy will be done on earth as it is in heaven.
+Give us this day our daily bread;""", """and forgive us our trespasses,
+as we forgive those who trespass against us;
+and lead us not into temptation,
+but deliver us from evil.""", """For the kingdom,
+the power and the glory are yours
+now and for ever.
+Amen."""]
+
+ave_maria_de = ["""Gegrüßet seist du, Maria, voll der Gnade,
+der Herr ist mit dir.
+Du bist gebenedeit unter den Frauen,
+und gebenedeit ist die Frucht deines Leibes, Jesus.""", """Heilige Maria, Mutter Gottes,
+bitte für uns Sünder
+jetzt und in der Stunde unseres Todes.
+Amen."""]
+
+ave_maria_la = ["""Ave Maria, gratia plena,
+Dominus tecum.
+Benedicta tu in mulieribus,
+et benedictus fructus ventris tui, Iesus.""", """Sancta Maria, Mater Dei,
+ora pro nobis peccatoribus
+nunc et in hora mortis nostrae.
+Amen"""]
+
+ave_maria_en = ["""Hail Mary, full of grace,
+the Lord is with thee.
+Blessed art thou amongst women,
+and blessed is the fruit of thy womb, Jesus.""", """Holy Mary, Mother of God,
+pray for us sinners,
+now and at the hour of our death.
+Amen."""]
+
+credo_de = ["""Ich glaube an Gott,
+den Vater, den Allmächtigen,
+den Schöpfer des Himmels und der Erde.""", """Und an Jesus Christus,
+seinen eingeborenen Sohn, unsern Herrn,
+empfangen durch den Heiligen Geist,
+geboren von der Jungfrau Maria,
+gelitten unter Pontius Pilatus,""", """gekreuzigt, gestorben und begraben,
+hinabgestiegen in das Reich des Todes,
+am dritten Tage auferstanden von den Toten,
+aufgefahren in den Himmel;""", """er sitzt zur Rechten Gottes,
+des allmächtigen Vaters;
+von dort wird er kommen,
+zu richten die Lebenden und die Toten.""", """Ich glaube an den Heiligen Geist,
+die heilige katholische Kirche,
+Gemeinschaft der Heiligen,
+Vergebung der Sünden,
+Auferstehung der Toten
+und das ewige Leben.
+Amen."""]
+
+credo_la = ["""Credo in Deum,
+Patrem omnipotentem,
+Creatorem caeli et terrae.""", """Et in Iesum Christum,
+Filium eius unicum, Dominum nostrum:
+qui conceptus est de Spiritu Sancto,
+natus ex Maria Virgine,
+passus sub Pontio Pilato,""", """crucifixus, mortuus, et sepultus,
+descendit ad inferos:
+tertia die resurrexit a mortuis;
+ascendit ad caelos;""", """sedet ad dexteram Dei
+Patris omnipotentis:
+inde venturus est
+iudicare vivos et mortuos.""", """Credo in Spiritum Sanctum,
+sanctam Ecclesiam catholicam,
+Sanctorum communionem,
+remissionem peccatorum,
+carnis resurrectionem,
+vitam aeternam.
+Amen."""]
+
+credo_en = [""" """, ]
